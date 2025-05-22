4a772aa15f330a899ea39b0d4e98b20e1259fbaf
Kuba <272264@student.pwr.edu.pl>
2025-05-22 23:52:22 +0200

    Inialized project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./app.css";
+import Home from './pages/Home/Home';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='app'>
+        <div className="appContainer">
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            {/* <Route path="/services" element={<Services />} /> */}
+            {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+          </Routes>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+        <div className="homeContainer">
+            Home
+        </div>
+    </div>
+  )
+}
+
+export default Home
