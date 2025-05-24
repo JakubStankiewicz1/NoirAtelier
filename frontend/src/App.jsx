5c04d583d8c86519e39072a6ef389a8314b9de4f
Kuba <272264@student.pwr.edu.pl>
2025-05-24 16:38:16 +0200

    Added PropertiesHero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./app.css";
 import Home from './pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Properties from './pages/Properties/Properties';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
 
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/properties" element={<Properties />} />
             {/* <Route path="/contact" element={<Contact />} /> */}
             {/* <Route path="/services" element={<Services />} /> */}
             {/* <Route path="/portfolio" element={<Portfolio />} /> */}
---
+++ frontend/src/pages/Properties/Properties.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./properties.css";
+import PropertiesHero from '../../components/PropertiesHero/PropertiesHero';
+
+const Properties = () => {
+  return (
+    <div className='properties'>
+        <PropertiesHero />
+    </div>
+  )
+}
+
+export default Properties
