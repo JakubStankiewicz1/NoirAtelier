35398a47fc50eecaef705fc1743321ea13b1870b
Kuba <272264@student.pwr.edu.pl>
2025-05-24 19:55:40 +0200

    Added subscribe section

---
+++ frontend/src/components/PropertiesCollectionSubscribe/PropertiesCollectionSubscribe.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import "./propertiesCollectionSubscribe.css";
+
+const PropertiesCollectionSubscribe = () => {
+  return (
+    <div className='propertiesCollectionSubscribe'>
+        <div className="propertiesCollectionSubscribeContainer">
+
+            {/* First Part */}
+            <div className="propertiesCollectionSubscribeContainerFirst">
+                <div className="propertiesCollectionSubscribeContainerFirstContainer">
+                    <p className="propertiesCollectionSubscribeContainerFirstContainerText">
+                        SUBSCRIBE
+                    </p>
+                </div>
+            </div>
+
+            {/* Second Part */}
+            <div className="propertiesCollectionSubscribeContainerSecond">
+                <div className="propertiesCollectionSubscribeContainerSecondContainer">
+                    <p className="propertiesCollectionSubscribeContainerSecondContainerText cormorant-garamond-regular">
+                        Stay tuned about new offer and promotions
+                    </p>
+                </div>
+            </div>
+
+            {/* Third Part */}
+            <div className="propertiesCollectionSubscribeContainerThird">
+                <div className="propertiesCollectionSubscribeContainerThirdContainer">
+                    <div className="propertiesCollectionSubscribeContainerThirdContainerOne">
+                        <div className="propertiesCollectionSubscribeContainerThirdContainerOneContainer">
+                            <input type="text" placeholder='Your Mail' className='propertiesCollectionSubscribeContainerThirdContainerOneContainerInput' />
+                        </div>
+                    </div>
+
+                    <div className="propertiesCollectionSubscribeContainerThirdContainerTwo">
+                        <div className="propertiesCollectionSubscribeContainerThirdContainerTwoContainer">
+                            <div className="propertiesCollectionSubscribeContainerThirdContainerTwoContainerButton">
+                                <div className="propertiesCollectionSubscribeContainerThirdContainerTwoContainerButtonContainer">
+                                    <p className="propertiesCollectionSubscribeContainerThirdContainerTwoContainerButtonContainerText">
+                                        Subscribe
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Fourth Part */}
+            <div className="propertiesCollectionSubscribeContainerFourth">
+                <div className="propertiesCollectionSubscribeContainerFourthContainer">
+                    <p className="propertiesCollectionSubscribeContainerFourthContainerText">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default PropertiesCollectionSubscribe
--- frontend/src/pages/Properties/Properties.jsx
+++ frontend/src/pages/Properties/Properties.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./properties.css";
 import PropertiesHero from '../../components/PropertiesHero/PropertiesHero';
 import PropertiesCollection from '../../components/PropertiesCollection/PropertiesCollection';
+import PropertiesCollectionSubscribe from '../../components/PropertiesCollectionSubscribe/PropertiesCollectionSubscribe';
 
 const Properties = () => {
   return (
     <div className='properties'>
         <PropertiesHero />
         <PropertiesCollection />
+        <PropertiesCollectionSubscribe />
     </div>
   )
 }
