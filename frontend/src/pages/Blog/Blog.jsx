66a3a6302f49f84069c1e09ba379d033b32f0ce6
Kuba <272264@student.pwr.edu.pl>
2025-05-25 14:22:24 +0200

    Added fotter and subscribe section to blog page

--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import "./blog.css";
 import BlogHeader from '../../components/BlogHeader/BlogHeader';
 import BlogCollection from '../../components/BlogCollection/BlogCollection';
+import PropertiesCollectionSubscribe from '../../components/PropertiesCollectionSubscribe/PropertiesCollectionSubscribe';
+import Fotter from '../../components/Fotter/Fotter';
 
 const Blog = () => {
   return (
     <div className='blog'>
         <BlogHeader />
         <BlogCollection />
+        <PropertiesCollectionSubscribe />
+        <Fotter />
     </div>
   )
 }
