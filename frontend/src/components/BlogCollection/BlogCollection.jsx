f2830e164619c4b6e445df8769aac6ace95d2a04
Kuba <272264@student.pwr.edu.pl>
2025-05-25 14:16:20 +0200

    Added Blogs component

---
+++ frontend/src/components/BlogCollection/BlogCollection.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import "./blogCollection.css";
+import { FaUser } from "react-icons/fa6";
+import { LuClock } from "react-icons/lu";
+import assets from '../../assets/assets';
+
+const BlogCollection = () => {
+  return (
+    <div className='blogCollection'>
+        <div className="blogCollectionContainer">
+
+            {/* Blog Element */}
+            <div className="blogCollectionContainerBlog">
+                <div className="blogCollectionContainerBlogContainer">
+                    {/* Left Part */}
+                    <div className="blogCollectionContainerBlogContainerLeft">
+                        <div className="blogCollectionContainerBlogContainerLeftContainer">
+                            <div className="blogCollectionContainerBlogContainerLeftImageWrapper">
+                                <img 
+                                    src={assets.FaqBenefitsImageOne} 
+                                    alt="Blog post featured image" 
+                                    className='blogCollectionContainerBlogContainerLeftContainerImage' 
+                                />
+                                <div className="blogCollectionContainerBlogContainerLeftOverlay"></div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    {/* Right Part */}
+                    <div className="blogCollectionContainerBlogContainerRight">
+                        <div className="blogCollectionContainerBlogContainerRightContainer">
+                            {/* Top Part */}
+                            <div className="blogCollectionContainerBlogContainerRightContainerTop">
+                                <div className="blogCollectionContainerBlogContainerRightContainerTopContainer">
+                                    <p className="blogCollectionContainerBlogContainerRightContainerTopContainerTextOne">
+                                        Published by
+                                    </p>
+
+                                    <FaUser className='blogCollectionContainerBlogContainerRightContainerTopContainerIconOne' />
+
+                                    <p className="blogCollectionContainerBlogContainerRightContainerTopContainerTextTow">
+                                        on
+                                    </p>
+
+                                    <LuClock className='blogCollectionContainerBlogContainerRightContainerTopContainerIconTwo' />
+
+                                    <p className="blogCollectionContainerBlogContainerRightContainerTopContainerTextThree">
+                                        October 5, 2022
+                                    </p>
+                                </div>
+                            </div>
+
+                            {/* Middle Part */}
+                            <div className="blogCollectionContainerBlogContainerRightContainerMiddle">
+                                <div className="blogCollectionContainerBlogContainerRightContainerMiddleContainer">
+                                    <p className="blogCollectionContainerBlogContainerRightContainerMiddleContainerText cormorant-garamond-regular">
+                                        Ligula amet taciti nullam pellentesque
+                                    </p>
+                                </div>
+                            </div>
+
+                            {/* Bottom Part */}
+                            <div className="blogCollectionContainerBlogContainerRightContainerBottom">
+                                <p className="blogCollectionContainerBlogContainerRightContainerBottomContainer">
+                                    <p className="blogCollectionContainerBlogContainerRightContainerBottomContainerText">
+                                        Suscipit condimentum facilisis tincidunt mollis fermentum si ante consequat faucibus pulvinar. Posuere nisl elementum consequat natoque nascetur sed. Est molestie leo aliquet accumsan magnis.
+                                    </p>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default BlogCollection
--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./blog.css";
 import BlogHeader from '../../components/BlogHeader/BlogHeader';
+import BlogCollection from '../../components/BlogCollection/BlogCollection';
 
 const Blog = () => {
   return (
     <div className='blog'>
         <BlogHeader />
+        <BlogCollection />
     </div>
   )
 }
