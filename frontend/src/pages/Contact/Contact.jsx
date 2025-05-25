4d2fa75968f5b0ad79b4c56f5775747c32a9795c
Kuba <272264@student.pwr.edu.pl>
2025-05-25 20:12:52 +0200

    Added fotter to contact page

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./contact.css";
 import ContactHeader from '../../components/ContactHeader/ContactHeader';
 import ContactForm from '../../components/ContactForm/ContactForm';
+import Fotter from '../../components/Fotter/Fotter';
 
 const Contact = () => {
   return (
     <div className='contact'>
         <ContactHeader />
         <ContactForm />
+        <Fotter />
     </div>
   )
 }
