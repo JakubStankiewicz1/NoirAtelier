b5ab30ea7bfccb9430e32ef2d9bccf8ac91f04ef
Kuba <272264@student.pwr.edu.pl>
2025-05-25 21:05:39 +0200

    Added homeVideo element

--- frontend/src/components/HomeVideo/HomeVideo.jsx
+++ frontend/src/components/HomeVideo/HomeVideo.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import "./homeVideo.css";
 
 import HomeVideoMP4 from "../../assets/HomeVideo.mp4";
+import { FaPlay } from "react-icons/fa";
 
 const HomeVideo = () => {
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className='homeVideo'>
@@ -18,58 +18,35 @@ const HomeVideo = () => {
                         muted
                         className="homeVideoContainerTopContainerVideo"
                     />
-                    {/* Przyciemnienie */}
-                    <div className="homeVideoOverlay" />
-                    {/* Tekst przewijający */}
-                    <div className="homeVideoMarquee">
-                      <span>
-                        Wyjątkowe miejsca na Twoje wydarzenie &nbsp;•&nbsp; Luksusowe apartamenty &nbsp;•&nbsp; Noir Atelier &nbsp;•&nbsp;
-                      </span>
-                      <span>
-                        Wyjątkowe miejsca na Twoje wydarzenie &nbsp;•&nbsp; Luksusowe apartamenty &nbsp;•&nbsp; Noir Atelier &nbsp;•&nbsp;
-                      </span>
-                    </div>
+ 
                     {/* Play button */}
-                    <button
-                      className="homeVideoPlayButton"
+                    <div
+                      className="homeVideoContainerTopContainerButton"
                       onClick={() => setShowModal(true)}
                       aria-label="Play video"
                     >
-                      <div className="playIcon"></div>
-                    </button>
+                      <FaPlay className='homeVideoContainerTopContainerButtonIcon' />
+                    </div>
+
+                    {/* Scrolling marquee */}
+                    <div className="homeVideoMarquee">
+                        <div className="homeVideoMarqueeInner">
+                            <p className="homeVideoMarqueeText">
+                                Explore our exclusive properties &nbsp;&nbsp;
+                            </p>
+                            <p className="homeVideoMarqueeText">
+                                Explore our exclusive properties &nbsp;&nbsp;
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className="homeVideoContainerBottom">
-                <div className="homeVideoContainerBottomText">
-                    <p className="homeVideoContainerBottomTextTitle cormorant-garamond-regular">
-                        Explore Our Unique Properties
-                    </p>
-                    <p className="homeVideoContainerBottomTextDescription cormorant-garamond-regular">
-                        Discover the perfect space for your next event or stay.
-                    </p>
-                </div>
             </div>
         </div>
-
-        {/* Modal fullscreen */}
-        {showModal && (
-          <div className="homeVideoModal">
-            <div className="homeVideoModalOverlay" onClick={() => setShowModal(false)} />
-            <div className="homeVideoModalContent">
-              <button className="homeVideoModalClose" onClick={() => setShowModal(false)}>&times;</button>
-              <video
-                src={HomeVideoMP4}
-                autoPlay
-                loop
-                controls
-                className="homeVideoModalVideo"
-              />
-            </div>
-          </div>
-        )}
     </div>
   )
 }
 
-export default HomeVideo
+export default HomeVideo;
